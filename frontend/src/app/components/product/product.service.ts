0667b732494a02fb693b163b2045ad816127c11d
Jackson Willian <jacksonwilliansilvaagostinho@gmail.com>
2020-12-06 09:53:45 -0300

    feat(frontend): atualizar product service para buscar por id e atualizar

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -26,6 +26,17 @@ export class ProductService {
     return this.http.get<Product[]>(this.baseUrl);
   }
   
+ 
+  readById(id: string): Observable<Product> {
+    let urlToGetProduct = `${this.baseUrl}/${id}`;
+    return this.http.get<Product>(urlToGetProduct);
+  }
+
+
+  update(product: Product): Observable<Product> {
+    let urlToUpdateProduct = `${this.baseUrl}/${product.id}`;
+    return this.http.put<Product>(urlToUpdateProduct, product);
+  }
 
   showMessage(msg: string): void {
     this.snackBar.open(msg, "X", {
